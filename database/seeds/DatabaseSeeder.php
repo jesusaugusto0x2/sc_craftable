1b1331b2cd7afb9d0a2ea62ce3baded6da5478d7
kvnsnchz <kvn9616@gmail.com>
2020-03-16 22:03:47 +0100

    agregando vistas de usuarios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(BanksSeeder::class);
+        $this->call(BankSeeder::class);
         $this->call(PaymentMethodSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(CampSeeder::class);
